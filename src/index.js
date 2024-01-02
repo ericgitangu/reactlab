4d446fc7b4e102879d1a90adc6427fe5db85b128
Eric <developer.ericgitangu@gmail.com>
2024-01-02 17:06:14 -0500

    Meta Android Developer Specialization - React basics final project, a simple react calculator. Final code refactor and code documentation.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import React from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+// @ts-ignore
+const root = ReactDOM.createRoot(rootElement);
 root.render(<App />);
